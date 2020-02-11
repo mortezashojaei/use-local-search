f6509af60a525303524b3340a196daaa05734548
morteza shojaei <mshojaei179@gmail.com>
2020-02-11 22:51:39 +0330

    add Usage section in ReadMe

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 A custom React Hook to search locally in array.
+
 ## Install
 
 `$ npm install use-local-search`
+
+## Usage
+
+`const { searchText,//value of search input
+setSearchText,//handler of search input
+searchedList //searched list (filtered by searchText value)
+} = useLocalSearch(list, listItem => listItem.name);//search base on name of each item in list
+
+<input value={searchText} onChange={setSearchText} />`
 
 ## License
 
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
 export const useLocalSearch = (list, callBack = item => item) => {
-  const [searchText, setSearchText ] = usState("");
+  const [searchText, setSearchText] = useState("");
   const searchedList =
     searchText.length > 0
       ? list.filter(item => String(callBack(item)).includes(searchText))
       : list;
 
-  return {searchText, setSearchText, searchedList};
+  return { searchText, setSearchText, searchedList };
 };
